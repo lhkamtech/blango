2cd0bd109d5465f3af1c52879ea7fff459f6f12d
Lawrence Kaminsky <lhkamin@gmail.com>
2024-02-06 00:46:15 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from blog.models import Tag, Post
+admin.site.register(Tag)
+
+# Custom approach (configure how the admin site behaves)
+class PostAdmin(admin.ModelAdmin):
+    # "Slugify" the title field into the slug field whenever the title
+    # field changes (inserts some JavaScript to do this)
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ["title", "slug", "published_at"]
+
+admin.site.register(Post, PostAdmin)
