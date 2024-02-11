099f3be16faa865ffb6099c398c91eb6c2b62f44
Lawrence Kaminsky <lhkamin@gmail.com>
2024-02-11 19:30:15 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.auth import get_user_model
+from blog.models import Post
 
 from django.utils.html import format_html
 
@@ -28,3 +29,24 @@ def author_details(author, current_user):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
