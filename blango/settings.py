29274b7b7a5ed9d6192b6bc68f4ab2201f0334a6
Lawrence Kaminsky <lhkamin@gmail.com>
2024-02-16 18:22:16 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -113,6 +113,16 @@ class Dev(Configuration):
         },
     ]
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
+
+
+
     # Crispy Forms
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
