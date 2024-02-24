124c8737a31a487661340555eb991492ed26b451
Lawrence Kaminsky <lhkamin@gmail.com>
2024-02-24 21:12:55 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -52,9 +52,11 @@ class Dev(Configuration):
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        'debug_toolbar',
     ]
 
     MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -153,6 +155,9 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    # For Debug Toolbar
+    INTERNAL_IPS = ["192.168.11.179"]
 
     LOGGING = {
         "version": 1,
