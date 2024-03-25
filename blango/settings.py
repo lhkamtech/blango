dd8017a9717ba3506f732c3c9c22a4e5506ad555
Lawrence Kaminsky <lhkamin@gmail.com>
2024-03-25 16:20:49 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -67,6 +67,7 @@ class Dev(Configuration):
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
+        'rest_framework',
         'allauth',
         'allauth.account',
         'allauth.socialaccount',
