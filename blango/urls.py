cae4484d19149efc4c581a83fc023198069f4a94
Lawrence Kaminsky <lhkamin@gmail.com>
2024-03-26 03:49:34 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
