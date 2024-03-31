75ad0dd372c02a7b8b01991c69b336d337289d45
Lawrence Kaminsky <lhkamin@gmail.com>
2024-03-31 02:32:55 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -69,6 +69,7 @@ class Dev(Configuration):
         'debug_toolbar',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg',
         'allauth',
         'allauth.account',
         'allauth.socialaccount',
@@ -158,6 +159,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],    
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
     # Crispy Forms
